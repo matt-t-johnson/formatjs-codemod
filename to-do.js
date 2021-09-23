b350a07b8df31a6c710d4384672a4f19f2ea7e63
Matt Johnson <matthew.t.johnson@avalara.com>
2021-09-22 21:57:37 -0400

    clean up

--- to-do.js
+++ to-do.js
@@ -1,7 +1,11 @@
 // TODO: Files in the /files-to-write folder need to be useable in projects that use import and projects that use require
 // TODO: add chalk to make CLI pretty
-// TODO: uninstall rollup? May not be needed for a CLI tool
 // TODO: implement default locale config
 // TODO: update config paths to run correctly from directory where reformatjs is run
 // TODO: update i18n files that get copied over to source project
 // TODO: add tests
+// TODO: other framework support
+// - typescript
+// - angular
+// - vue
+// - svelte
